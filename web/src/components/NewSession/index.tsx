28698aaa18961d927f6c596837778961bcd593eb
swear01 <stanley.yellow1@gmail.com>
2026-08-11 16:00:31 +0800

    carry: 14-pr-1421 (resolved from v0.27.2.2)

--- web/src/components/NewSession/index.tsx
+++ web/src/components/NewSession/index.tsx
@@ -1714,7 +1714,7 @@ export function NewSession(props: {
             && cursorModelsState.isLoading)
         || (agent === 'grok'
             && deferredDirectory !== ''
-            && (model !== 'auto' || effort !== 'auto')
+            && (model !== 'auto' || effort !== 'auto' || grokPermissionMode === 'auto')
             && (
                 deferredDirectoryExists === undefined
                 || (deferredDirectoryExists === true && grokModelsState.isLoading)
